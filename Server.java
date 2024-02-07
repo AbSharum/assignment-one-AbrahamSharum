0d3c6a439b7d26663996b8b21d4af06099f8d2f3
Abraham Sharum <asharu02@uafs.edu>
2024-02-07 10:55:18 -0600

    supposed finished

--- Server.java
+++ Server.java
@@ -1,26 +1,30 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Date;
 
 
 import java.io.OutputStream;
 
-public class Server{
+public class Server {
     private ServerSocket serverSocket;
     private Socket clientSocket;
     private int portNo;
+    
 
-    public Server(int portNo){
-        this.portNo=portNo;
+    public Server(int portNo) {
+        this.portNo = portNo;
     }
 
     private void processConnection() throws IOException{
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         OutputStream out = clientSocket.getOutputStream();
-
 
         //*** Application Protocol *****
         //****************************** */
@@ -28,36 +32,43 @@ public class Server{
         String buffer = "";
         String line;
 
-
-        while((line = in.readLine()) != null ){
-            if(line.equalsIgnoreCase("quit") || line.equals("")){
+        while((line = in.readLine()) != null ) {
+            if(line.equalsIgnoreCase("quit") || line.equals("")) {
                 break;
             }
-
             buffer += line+"\n";
-
         }
-        System.out.println(buffer);
-        byte[] data = buffer.getBytes();
+
+        String filePath = buffer.split(" ")[1]; 
+
+        if (filePath.equals("/")) {
+            filePath = "/home.html";
+        }
+
+        byte[] file = Files.readAllBytes(Paths.get(System.getProperty("user.dir")+"\\docroot"+filePath));
+
         String response = 
         "HTTP/1.1 200 OK" + "\r\n" +
         "Date: " + new Date() + "\r\n" +
         "Content-Type: text/html" + "\r\n" + 
-        "Content-Length: " + (data.length) + "\r\n" +
-        "Last-Modified : ";
-        
+        "Content-Length: " + (file.length) + "\r\n" +
+        "Connection: close\r\n\r\n";
+
         out.write(response.getBytes());
-        System.out.print(response);
+        out.write(file);
+        out.write("\r\n".getBytes());
+        out.flush();
+                
         in.close();
         out.close();
     }
 
-    public void run() throws IOException{
+    public void run() throws IOException {
         boolean running = true;
        
         serverSocket = new ServerSocket(portNo);
-        System.out.printf("Listen on Port: %d\n",portNo);
-        while(running){
+        System.out.printf("Listen on Port: %d\n", portNo);
+        while(running) {
             clientSocket = serverSocket.accept();
             //** Application Protocol
             processConnection();
@@ -65,7 +76,8 @@ public class Server{
         }
         serverSocket.close();
     }
-    public static void main(String[] args0) throws IOException{
+    
+    public static void main(String[] args0) throws IOException {
         Server server = new Server(8080);
         server.run();
     }
